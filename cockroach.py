37795557fe6fc51555f8bb1960499e35b4cd56cd
Surya Narayan <surya.narayan0707@gmail.com>
2023-11-22 17:01:45 -0700

    insert statement functions
    changed directory name of input data and added insert statements for some tables

--- cockroach.py
+++ cockroach.py
@@ -117,12 +117,128 @@ def create_database_tables():
     except (Exception, psycopg2.DatabaseError) as error:
         print(f"Error: {error}")
 
+    #entering data here 
+
     finally:
         # Close the cursor and connection
         cursor.close()
         conn.close()
 
+import os
+import csv
+import psycopg2
+
+def insert_supplier_data_from_csv(csv_file_path):
+    conn = psycopg2.connect(os.environ["DATABASE_URL"])
+
+
+    try:
+        with conn.cursor() as cur:
+            with open(csv_file_path, 'r') as csvfile:
+                csvreader = csv.DictReader(csvfile)
+
+                for row in csvreader:
+                    # Insert data into the Supplier table
+                    cur.execute("""
+                        INSERT INTO Supplier (SupplierID, SupplierName, ContactInfo, Address)
+                        VALUES (%s, %s, %s, %s)
+                    """, (row['SupplierID'], row['SupplierName'], row['ContactInfo'], row['Address']))
+
+            conn.commit()
+            print("Data inserted into the Supplier table successfully!")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        conn.close()
+
+def insert_manufacturer_data_from_csv(csv_file_path):
+    conn = psycopg2.connect(os.environ["DATABASE_URL"])
+    try:
+        with conn.cursor() as cur:
+            with open(csv_file_path, 'r') as csvfile:
+                csvreader = csv.DictReader(csvfile)
+
+                for row in csvreader:
+                    # Insert data into the Manufacturer table
+                    cur.execute("""
+                        INSERT INTO Manufacturer (ManufacturerID, ManufacturerName, Country, ContactInfo)
+                        VALUES (%s, %s, %s, %s)
+                    """, (row['ManufactureID'], row['ManufactureName'], row['Country'], row['Contact']))
+
+            conn.commit()
+            print("Data inserted into the Manufacturer table successfully!")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        conn.close()
+
+
+def insert_mobilephone_data_from_csv(csv_file_path):
+    conn = psycopg2.connect(os.environ["DATABASE_URL"])
+
+    try:
+        with conn.cursor() as cur:
+            with open(csv_file_path, 'r') as csvfile:
+                csvreader = csv.DictReader(csvfile)
+
+                for row in csvreader:
+                    # Extract year from ReleaseYear and construct a date
+                    release_year = int(row['ReleaseYear'])
+                    release_date = f"{release_year}-01-01"
+
+                    # Insert data into the MobilePhone table
+                    cur.execute("""
+                        INSERT INTO MobilePhone (MobilePhoneID, ManufacturerID, ModelName, Price, ReleaseDate)
+                        VALUES (%s, %s, %s, %s, %s)
+                    """, (row['MobilePhoneID'], row['ManufactureID'], row['ModelName'], row['Price'], release_date))
+
+            conn.commit()
+            print("Data inserted into the MobilePhone table successfully!")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        conn.close()
+
+def insert_warehouse_data_from_csv(csv_file_path):
+    conn = psycopg2.connect(os.environ["DATABASE_URL"])
+
+    try:
+        with conn.cursor() as cur:
+            with open(csv_file_path, 'r') as csvfile:
+                csvreader = csv.DictReader(csvfile)
+
+                for row in csvreader:
+                    # Insert data into the Warehouse table
+                    cur.execute("""
+                        INSERT INTO Warehouse (WarehouseID, Location, Capacity)
+                        VALUES (%s, %s, %s)
+                    """, (row['WarehouseID'], row['Location'], row['Capacity']))
+
+            conn.commit()
+            print("Data inserted into the Warehouse table successfully!")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        conn.close()
+
 
 
 if __name__ == "__main__":
-    create_database_tables()
+    # create_database_tables()
+    # insert_supplier_data_from_csv('InputData/Supplier.csv')
+    # insert_manufacturer_data_from_csv('InputData/Manufacturer.csv')
+    # insert_mobilephone_data_from_csv('InputData/MobilePhone.csv')
+    # insert_warehouse_data_from_csv('InputData/Warehouse.csv')
+    
+
+
+
+
